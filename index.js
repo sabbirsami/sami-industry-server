d8dce37ce74c0a43c0106fd43bca70d4ace1ff07
SabbirSami <smd71430@gmail.com>
2022-05-22 20:41:12 +0600

    connected with mongodb

--- index.js
+++ index.js
@@ -4,9 +4,27 @@ require("dotenv").config();
 const jwt = require("jsonwebtoken");
 const cors = require("cors");
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.4vlxs.mongodb.net/?retryWrites=true&w=majority`;
 
 app.use(cors());
 app.use(express.json());
+
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+    try {
+        await client.connect();
+        const productCollection = client
+            .db("samiIndustry")
+            .collection("products");
+    } finally {
+    }
+}
 
 app.get("/", (req, res) => {
     res.send("Sami Industry's server");
