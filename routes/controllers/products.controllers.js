ad5cde6f797dc72900cab7d06353eb17c89ccd40
SabbirSami <smd71430@gmail.com>
2022-09-10 11:35:28 +0600

    update product api done

--- routes/controllers/products.controllers.js
+++ routes/controllers/products.controllers.js
@@ -14,16 +14,13 @@ module.exports.saveAProduct = async (req, res, next) => {
     try {
         const db = getDb();
         const product = req.body;
-
         const result = await db.collection("products").insertOne(product);
         console.log(result);
-
         if (!result.insertedId) {
             return res
                 .status(400)
                 .send({ status: false, error: "Something went wrong!" });
         }
-
         res.send({
             success: true,
             message: `Product added with id: ${result.insertedId}`,
@@ -37,27 +34,47 @@ module.exports.getProductDetail = async (req, res, next) => {
     try {
         const db = getDb();
         const { id } = req.params;
-
         if (!ObjectId.isValid(id)) {
             return res
                 .status(400)
                 .json({ success: false, error: "Not a valid Product id." });
         }
-
         const product = await db
             .collection("products")
             .findOne({ _id: ObjectId(id) });
-
         if (!product) {
+            return res.status(400).json({
+                success: false,
+                error: "Couldn't find a Product with this id",
+            });
+        }
+        res.status(200).json({ success: true, data: product });
+    } catch (error) {
+        next(error);
+    }
+};
+
+module.exports.updateProduct = async (req, res, next) => {
+    try {
+        const db = getDb();
+        const { id } = req.params;
+        if (!ObjectId.isValid(id)) {
             return res
                 .status(400)
-                .json({
-                    success: false,
-                    error: "Couldn't find a Product with this id",
-                });
+                .json({ success: false, error: "Not a valid tool id." });
         }
-
-        res.status(200).json({ success: true, data: product });
+        const tool = await db
+            .collection("tools")
+            .updateOne({ _id: ObjectId(id) }, { $set: req.body });
+        if (!tool.modifiedCount) {
+            return res
+                .status(400)
+                .json({ success: false, error: "Couldn't update the tool" });
+        }
+        res.status(200).json({
+            success: true,
+            message: "Successfully updated the tool",
+        });
     } catch (error) {
         next(error);
     }
