adb12ec9370353d5059d835272328f754e8987e2
SabbirSami <smd71430@gmail.com>
2022-09-10 11:30:27 +0600

    get a single product api done

--- routes/controllers/products.controllers.js
+++ routes/controllers/products.controllers.js
@@ -26,8 +26,38 @@ module.exports.saveAProduct = async (req, res, next) => {
 
         res.send({
             success: true,
-            message: `Tool added with id: ${result.insertedId}`,
+            message: `Product added with id: ${result.insertedId}`,
         });
+    } catch (error) {
+        next(error);
+    }
+};
+
+module.exports.getProductDetail = async (req, res, next) => {
+    try {
+        const db = getDb();
+        const { id } = req.params;
+
+        if (!ObjectId.isValid(id)) {
+            return res
+                .status(400)
+                .json({ success: false, error: "Not a valid Product id." });
+        }
+
+        const product = await db
+            .collection("products")
+            .findOne({ _id: ObjectId(id) });
+
+        if (!product) {
+            return res
+                .status(400)
+                .json({
+                    success: false,
+                    error: "Couldn't find a Product with this id",
+                });
+        }
+
+        res.status(200).json({ success: true, data: product });
     } catch (error) {
         next(error);
     }
