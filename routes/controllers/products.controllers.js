fee2950c2c1c1f878c432083453d29643ab6a153
SabbirSami <smd71430@gmail.com>
2022-09-11 22:15:25 +0600

    test somthing

--- routes/controllers/products.controllers.js
+++ routes/controllers/products.controllers.js
@@ -105,3 +105,19 @@ module.exports.deleteProduct = async (req, res, next) => {
         next(error);
     }
 };
+
+module.exports.test = async (req, res, next) => {
+    for (let i = 0; i < 100000; i++) {
+        const db = getDb();
+        db.collection("test").insertOne({ name: `test ${i}`, age: i });
+    }
+};
+module.exports.testGet = async (req, res, next) => {
+    const db = getDb();
+
+    const result = await db
+        .collection("test")
+        .find({ name: "test 99999" })
+        .toArray();
+    res.json(result);
+};
